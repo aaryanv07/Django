87560e535d4cedee18696aff74188bba4551fc0d
aaryanv07 <aaryanvedpathak@gmail.com>
2026-08-12 14:21:18 +0530

    First commit

--- myProject2/blog/urls.py
+++ myProject2/blog/urls.py
@@ -8,3 +8,6 @@ urlpatterns=[
     re_path(r'^article/(?P<year>[0-9]{4})/$',views.articles_by_year,name='articles_by_year'),
     path('article/<int:year>/<int:month>/<int:day>/',views.article_details,name='article_details')
     ]
+
+
+# http://127.0.0.1:8000/blog/article/2025/04/5/
--- myProject2/blog/views.py
+++ myProject2/blog/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 
-def post_details(request, post_id):
+def post_details(request, post_id): 
     return HttpResponse(f"<h1>Post Details page for ID: {post_id}</h1>")
 def user_profile(request,username):
     return HttpResponse(f"<h1>User Profile page for Username: {username}</h1>")
@@ -10,4 +10,6 @@ def articles_by_year(request,year):
 #     return HttpResponse(f'<h1>Articles page for year : {year} and month : {month}</h1>')
 def article_details(request,**kwargs):
     return HttpResponse(f'<h1>Data: {kwargs}</h1>')
+
+# We are accepting the arguments in the url 
     
